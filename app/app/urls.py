2970a997fcbf23a03f6f802a7d99985787bbff80
Mark Seaman <seaman@seaman-hammer>
2014-09-16 14:57:29 -0600

    Fix media loading

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,6 +3,8 @@
 
 from django.conf.urls import patterns, include, url
 
+media_dir   = { 'document_root' : 'media/'}
+
 urlpatterns = patterns(
     '',
 
@@ -10,19 +12,19 @@ urlpatterns = patterns(
     (r'^login', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),  
     (r'^logout', 'app.views.logout_view'),
 
-    # doc
-    url(r'^',         include('doc.urls')),
-
     # task
     url(r'^project/', include('task.project_urls')),
     url(r'^time/',    include('task.time_urls')),
 
     # other
+    url(r'^media/(?P<path>.*)$',    'django.views.static.serve', media_dir),
 
     url(r'^home$', 'app.views.home', name='/'),
     url(r'^user$', 'app.views.user', name='user'),
     url(r'^test$', 'app.views.test_view', name='test'),
     url(r'^no_access$', 'app.views.no_access'),
 
+    # doc
+    url(r'^',         include('doc.urls')),
 
 )
