69f2ab79e68562d277914f1732fcd4018bf96e67
Mark Seaman <seaman@seaman-hammer>
2015-09-23 16:15:49 -0600

    Hello

--- ocean/hello/views.py
+++ ocean/hello/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! This is our first view.")
+    return HttpResponse("Hello, Mark! Welcome to the water.")
