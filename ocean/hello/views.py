ee781c4b2be0f15049ee7f94ad712a84340e7897
Mark Seaman <seaman@seaman-hammer>
2015-09-23 16:32:19 -0600

    Move static files

--- ocean/hello/views.py
+++ ocean/hello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import listdir
+
 
 def index(request):
-    return HttpResponse("Hello, Mark! Welcome to the water.")
+    files = listdir('/home/django/django_project')
+    return HttpResponse("Hello, Mark! Welcome to the water.\n"+' ,'.join(files))
