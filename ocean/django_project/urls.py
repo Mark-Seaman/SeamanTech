f106b60f61a6cf452a8a3ab9077fa767395f2709
Mark Seaman <seaman@seaman-hammer>
2015-09-24 12:28:12 -0600

    Remove old code

--- ocean/django_project/urls.py
+++ ocean/django_project/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-from hello import views
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', views.index, name='index'),
     url(r'^',         include('doc.urls')),
 )
 
--- ocean/hello/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from os import listdir
-
-
-def index(request):
-    #files = listdir('/home/django/django_project')
-    files = listdir('/home/seaman/Projects/seamantech/ocean/')
-    return HttpResponse("Hello, Mark! Welcome to the water.\n"+' ,'.join(files))
